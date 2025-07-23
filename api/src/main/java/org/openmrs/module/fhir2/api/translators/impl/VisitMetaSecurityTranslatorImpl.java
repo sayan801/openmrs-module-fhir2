fdbf6677e38c514a575fa30a507fb8d3fc0a8e8e
suman-technicise <dustu9944it@gmail.com>
2025-07-23 17:29:20 +0530

    meta security system, code and display value added

--- api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/VisitMetaSecurityTranslatorImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/translators/impl/VisitMetaSecurityTranslatorImpl.java
@@ -37,15 +37,26 @@ public class VisitMetaSecurityTranslatorImpl implements VisitMetaSecurityTransla
 		
 		List<Coding> securityTags = new ArrayList<>();
 		
-		// set fhir meta.security from encounter- visit- attribute ==============================		
+		// set fhir meta.security from visit- attribute ==============================		
 		
 		visit.getAttributes().forEach(attr -> {
 			String attrTypeName = attr.getAttributeType().getName();
 			if ("security".equalsIgnoreCase(attrTypeName)) {
 				Object value = attr.getValue();
 				String displayValue = value != null ? value.toString() : "Restricted";
+				String display = "";
+				if (displayValue.contains(":")) {
+					String[] parts = displayValue.split(":", 2);
+					if (parts.length == 2) {
+						display = parts[1].trim();
+					} else {
+						display = displayValue;
+					}
+				}
+				String code = getSecurityCodeFromDisplay(display);
+				
 				Coding coding = new Coding().setSystem("http://terminology.hl7.org/CodeSystem/v3-Confidentiality")
-				        .setCode(attr.getUuid()).setDisplay(attrTypeName + ": " + displayValue);
+				        .setCode(code).setDisplay(display);
 				securityTags.add(coding);
 			}
 		});
@@ -77,20 +88,25 @@ public class VisitMetaSecurityTranslatorImpl implements VisitMetaSecurityTransla
 				
 				attribute.setAttributeType(attrType);
 				attribute.setUuid(coding.getCode());
+				attribute.setValue(display);
+				attribute.setValueReferenceInternal(display);
 				
 				// Extract dynamic value from display
-				String[] parts = display.split(":", 2);
-				if (parts.length == 2) {
-					String value = parts[1].trim();
-					
-					// Set both value and valueReferenceInternal dynamically
-					attribute.setValue(value);
-					attribute.setValueReferenceInternal(value);
-				} else {
-					// fallback if display is malformed
-					attribute.setValue("Restricted");
-					attribute.setValueReferenceInternal("Restricted");
-				}
+				// String[] parts = display.split(":", 2);
+				// if (parts.length == 2) 
+				// {
+				// 	String value = parts[1].trim();
+				
+				// 	// Set both value and valueReferenceInternal dynamically
+				// 	attribute.setValue(value);
+				// 	attribute.setValueReferenceInternal(value);
+				// }
+				//  else 
+				//  {
+				// 	// fallback if display is malformed
+				// 	attribute.setValue("Restricted");
+				// 	attribute.setValueReferenceInternal("Restricted");
+				// }
 				
 				// Add the attribute to the visit
 				visit.addAttribute(attribute);
@@ -101,6 +117,27 @@ public class VisitMetaSecurityTranslatorImpl implements VisitMetaSecurityTransla
 		}
 		
 		return visit;
+	}
+	
+	private String getSecurityCodeFromDisplay(String display) {
+		if (display == null)
+			return "U";
+		switch (display.toLowerCase()) {
+			case "low":
+				return "L";
+			case "moderate":
+				return "M";
+			case "normal":
+				return "N";
+			case "restricted":
+				return "R";
+			case "unrestricted":
+				return "U";
+			case "very restricted":
+				return "V";
+			default:
+				return "R";
+		}
 	}
 	
 }
